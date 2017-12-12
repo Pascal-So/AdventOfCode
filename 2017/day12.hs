f88035777818f18d2a3dc8b5cb2dfa8a7a1a4a21
Pascal Sommer <p@pascalsommer.ch>
2017-12-12 09:57:01 +0100

    solve day 12

---
+++ 2017/day12.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE ViewPatterns #-}
+
+import Data.Graph.Inductive
+
+solveA :: Gr n e -> Int
+solveA = length . dfs [0]
+
+solveB :: Gr n e -> Int
+solveB = length . components
+
+parseGraph :: String -> Gr () ()
+parseGraph str = mkUGraph nodes edges
+    where
+        list = map (map read . words . filter (`elem` (' ' : ['0' .. '9']))) . lines $ str
+        nodes = map head list
+        edges = concat $ map (\(x:xs) -> map ((,) x) xs) list
+
+main = do
+    input <- parseGraph <$> getContents
+    print $ solveB input
